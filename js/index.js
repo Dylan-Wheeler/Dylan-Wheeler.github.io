acf98081a0b9b6a588a08703af028fb58ce884df
Dylan-Wheeler <dwheeler1127@gmail.com>
2021-04-14 16:18:34 -0600

    Optimized code for final project

--- js/index.js
+++ js/index.js
@@ -6,125 +6,28 @@ var melody = false;
 
 function updateAudio() {
     var audio_element = document.getElementById("final_audio");
-    var audio_id = getAudioID();
+    var audio_path = getAudioPath();
     audio_element.pause();
     var current_time = audio_element.currentTime;
 
-    switch (audio_id) {
-        case '00000':
-            audio_element.setAttribute('src', '../audio/00000.wav');
-            break;
-        case '00001':
-            audio_element.setAttribute('src', '../audio/00001.wav');
-            break;
-        case '00010':
-            audio_element.setAttribute('src', '../audio/00010.wav');
-            break;
-        case '00100':
-            audio_element.setAttribute('src', '../audio/00100.wav');
-            break;
-        case '01000':
-            audio_element.setAttribute('src', '../audio/01000.wav');
-            break;
-        case '10000':
-            audio_element.setAttribute('src', '../audio/10000.wav');
-            break;
-        case '00011':
-            audio_element.setAttribute('src', '../audio/00011.wav');
-            break;
-        case '00101':
-            audio_element.setAttribute('src', '../audio/00101.wav');
-            break;
-        case '01001':
-            audio_element.setAttribute('src', '../audio/01001.wav');
-            break;
-        case '10001':
-            audio_element.setAttribute('src', '../audio/10001.wav');
-            break;
-        case '00110':
-            audio_element.setAttribute('src', '../audio/00110.wav');
-            break;
-        case '01010':
-            audio_element.setAttribute('src', '../audio/01010.wav');
-            break;
-        case '10010':
-            audio_element.setAttribute('src', '../audio/10010.wav');
-            break;
-        case '01100':
-            audio_element.setAttribute('src', '../audio/01100.wav');
-            break;
-        case '10100':
-            audio_element.setAttribute('src', '../audio/10100.wav');
-            break;
-        case '11000':
-            audio_element.setAttribute('src', '../audio/11000.wav');
-            break;
-        case '00111':
-            audio_element.setAttribute('src', '../audio/00111.wav');
-            break;
-        case '01011':
-            audio_element.setAttribute('src', '../audio/01011.wav');
-            break;
-        case '10011':
-            audio_element.setAttribute('src', '../audio/10011.wav');
-            break;
-        case '01101':
-            audio_element.setAttribute('src', '../audio/01101.wav');
-            break;
-        case '10101':
-            audio_element.setAttribute('src', '../audio/10101.wav');
-            break;
-        case '11001':
-            audio_element.setAttribute('src', '../audio/11001.wav');
-            break;
-        case '01110':
-            audio_element.setAttribute('src', '../audio/01110.wav');
-            break;
-        case '10110':
-            audio_element.setAttribute('src', '../audio/10110.wav');
-            break;
-        case '11010':
-            audio_element.setAttribute('src', '../audio/11010.wav');
-            break;
-        case '11100':
-            audio_element.setAttribute('src', '../audio/11100.wav');
-            break;
-        case '01111':
-            audio_element.setAttribute('src', '../audio/01111.wav');
-            break;
-        case '10111':
-            audio_element.setAttribute('src', '../audio/10111.wav');
-            break;
-        case '11011':
-            audio_element.setAttribute('src', '../audio/11011.wav');
-            break;
-        case '11101':
-            audio_element.setAttribute('src', '../audio/11101.wav');
-            break;
-        case '11110':
-            audio_element.setAttribute('src', '../audio/11110.wav');
-            break;
-        case '11111':
-            audio_element.setAttribute('src', '../audio/11111.wav');
-            break;
-        default:
-            break;
-    }
+    audio_element.setAttribute('src', audio_path);
 
     audio_element.currentTime = current_time;
-    if (audio_id != '00000') audio_element.play();
+    if (audio_path != '00000') audio_element.play();
 }
 
-function getAudioID() {
-    var placeholder_id = '';
+function getAudioPath() {
+    var placeholder_path = '../audio/';
 
-    drums ? placeholder_id += '1' : placeholder_id += '0';
-    bass ? placeholder_id += '1' : placeholder_id += '0';
-    synth ? placeholder_id += '1' : placeholder_id += '0';
-    piano ? placeholder_id += '1' : placeholder_id += '0';
-    melody ? placeholder_id += '1' : placeholder_id += '0';
+    drums ? placeholder_path += '1' : placeholder_path += '0';
+    bass ? placeholder_path += '1' : placeholder_path += '0';
+    synth ? placeholder_path += '1' : placeholder_path += '0';
+    piano ? placeholder_path += '1' : placeholder_path += '0';
+    melody ? placeholder_path += '1' : placeholder_path += '0';
 
-    return placeholder_id;
+    placeholder_path += '.wav';
+
+    return placeholder_path;
 }
 
 document.addEventListener('DOMContentLoaded', function () {
