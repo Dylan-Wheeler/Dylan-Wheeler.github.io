eb3d818d8719e8d5842835c93d14d4f4d5a0e685
Dylan Wheeler <dwheeler1127@gmail.com>
2024-05-24 11:16:37 -0700

    Add scroll to top button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,29 +3,34 @@
 import React, { useState, useEffect } from 'react'
 import Navigation from './Navigation'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
 
 function Header() {
-  const pathname = usePathname();
-
-  const [scrollTop, setScrollTop] = useState(0);
+  const [atPageTop, setPageTop] = useState(0);
   useEffect(() => {
     const onScroll = (e: any) => {
-      setScrollTop(e.target.documentElement.scrollTop);
+      setPageTop(e.target.documentElement.scrollTop);
     }
     window.addEventListener('scroll', onScroll);
-  }, [scrollTop])
+  }, [atPageTop])
 
   const [isSmallDevice, setSmallDevice] = useState(false);
   useEffect(() => {
     setSmallDevice(window.innerWidth < 700)
   }, [])
 
+  const setScrollTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    })
+  }
+
   return (
-    <header className={ scrollTop > 80 || !isSmallDevice && scrollTop > 45 ? "smaller-header" : "" }>
+    <header className={ atPageTop > 80 || !isSmallDevice && atPageTop > 45 ? "smaller-header" : "" }>
         <div id='header-background' aria-hidden='true'></div>
         <Link href={`/`} className='home-link'><h1><span className='large-header-link'>Dylan Wheeler</span><span className='small-header-link'>DW</span></h1></Link>
         <Navigation />
+        <button id='scroll-top' className={atPageTop > 300 ? '' : 'removed'} onClick={setScrollTop}><span className="material-symbols-outlined">arrow_upward</span></button>
     </header>
   )
 }
