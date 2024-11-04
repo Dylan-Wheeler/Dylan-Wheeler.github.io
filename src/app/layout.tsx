823bb00a1e4605a52c6e55b83440a56fed7a51a3
Dylan Wheeler <dwheeler1127@gmail.com>
2024-11-03 22:23:34 -0800

    Update content font + cleanup font styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,16 @@ import "./globals.scss";
 import "./prose.scss";
 import Header from "@/components/Header";
 import { ThemeProvider } from "./providers";
-import { Inter, Crimson_Text, Nunito } from "next/font/google";
+import { Inter, Crimson_Text, Nunito, Benne, Habibi, Metamorphous } from "next/font/google";
  
 // Fonts
 const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
 const crimson_text = Crimson_Text({ subsets: ["latin"], weight: "600", variable: '--font-crimson-text'});
 const nunito = Nunito({ subsets: ["latin"], weight: "400", variable: '--font-nunito'});
 const nunito_light = Nunito({ subsets: ["latin"], weight: "300", variable: '--font-nunito-light'});
+const benne = Benne({ subsets: ["latin"], weight: "400", variable: '--font-benne'});
+const habibi = Habibi({ subsets: ["latin"], weight: "400", variable: '--font-habibi'});
+const metamorphous = Metamorphous({ subsets: ["latin"], weight: "400", variable: '--font-metamorphous'});
 
 export const metadata: Metadata = {
   title: "Dylan Wheeler",
@@ -23,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${inter.variable} ${crimson_text.variable} ${nunito.variable} ${nunito_light.variable}`}>
+    <html lang="en" className={`${inter.variable} ${crimson_text.variable} ${nunito.variable} ${nunito_light.variable} ${benne.variable} ${habibi.variable} ${metamorphous.variable}`}>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
