4a93f6f2f447e33a4591975808b9a58268378a65
Dylan Wheeler <dwheeler1127@gmail.com>
2025-01-23 19:31:50 -0800

    Add content & styling for 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import Article from "@/components/Article";
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <div className="not-found">
+          <h2>The page could not be found</h2>
+        </div>
+      </main>
+    </>
+  );
+}
