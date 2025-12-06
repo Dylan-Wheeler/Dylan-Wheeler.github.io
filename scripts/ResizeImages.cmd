eca456c104a55f6219c489336f6a84f1c9813a05
Dylan Wheeler <dwheeler1127@gmail.com>
2025-12-06 00:31:21 -0800

    Add ResizeImages script and update photos page images

---
+++ scripts/ResizeImages.cmd
@@ -0,0 +1,2 @@
+:: This is a comment, it's ignored by the interpreter
+magick ./scripts/sourcePictures/*.jpg -resize 1080x1080 ./public/photos/resized/resized-%d.jpg
--- src/app/photography/util/photosUtils.ts
+++ src/app/photography/util/photosUtils.ts
@@ -2,10 +2,10 @@ import path from 'path'
 import fs from 'fs'
 
 export const getPhotos: () => string[] = () => {
-  const folder = path.join(process.cwd(), 'public/photos/compressed')
+  const folder = path.join(process.cwd(), 'public/photos/resized')
   const photos = fs.readdirSync(folder)
 
   return photos.map(photo => {
-    return path.join("/photos/compressed", photo)
+    return path.join("/photos/resized", photo)
   })
 }
